90e63e922eac77cef1ff72219f8db068360c227d
mniek <mniek890@o2.pl>
2019-10-13 13:46:45 +0200

    quick program t new patient

---
+++ freeCodeCamp/2nd/vars.py
@@ -0,0 +1,10 @@
+name = "John Smith"
+age = 20
+newPatient = True
+
+name = input( "Name and surname? ")
+age = input("Whats ur age? ")
+newPatient = input("new patient? ")
+print(name)
+print(age)
+print(newPatient)
